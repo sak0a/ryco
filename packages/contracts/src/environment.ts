54de26d0553fdf7f0c37c33b5ee20915f4872f92
sak0a <laurinfrank03@gmail.com>
2026-07-31 07:50:14 +0200

    feat(contracts): define thread settlement lifecycle

--- packages/contracts/src/environment.ts
+++ packages/contracts/src/environment.ts
@@ -21,6 +21,7 @@ export type ExecutionEnvironmentPlatform = typeof ExecutionEnvironmentPlatform.T
 
 export const ExecutionEnvironmentCapabilities = Schema.Struct({
   repositoryIdentity: Schema.Boolean.pipe(Schema.withDecodingDefault(Effect.succeed(false))),
+  threadSettlement: Schema.Boolean.pipe(Schema.withDecodingDefault(Effect.succeed(false))),
 });
 export type ExecutionEnvironmentCapabilities = typeof ExecutionEnvironmentCapabilities.Type;
 
